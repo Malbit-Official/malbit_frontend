dab22bbbe0245187b24e9a92cee2d801a5c8b19b
ch0412 <pce0412010@gmail.com>
2026-05-10 03:17:21 +0900

    design: Update home screen UI/UX

--- lib/main.dart
+++ lib/main.dart
@@ -20,6 +20,7 @@ void main() async {
     nativeAppKey: 'f90beb4e45400ac7959f9e2929295180',
   );
 
+  /*
   // ✅ 로그인 상태 확인 로직 복구
   final storage = const FlutterSecureStorage();
   final accessToken = await storage.read(key: 'accessToken');
@@ -32,9 +33,9 @@ void main() async {
   } else {
     log("로그인 필요");
     initialRoute = '/login';
-  }
+  }*/
 
-  runApp(MyApp(initialRoute: initialRoute));
+  runApp(MyApp(initialRoute: '/home'));
 }
 
 class MyApp extends StatelessWidget {
